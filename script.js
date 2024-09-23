e3c58e8b4659e652c3e6517d960e9374087bca3e
ArtemRudomanenko <artemrudomanenko@gmail.com>
2024-09-23 18:00:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+//2
+// for(let i =1;i<100;i++){
+//     if(i%3==0) {
+//         console.log('\n'+i)
+//     }
+// }
+
+//3
+// let a = parseInt(prompt("Input ur number -> "));
+// let b=1;
+// for(let i=2;i<=a;i++){
+//     b*=i;
+// }
+// alert(b)
+
+//1
+// let max = 100;
+// let min = 0;
+// let a;
+
+// alert("Input ur number from 0 to 100 ->");
+
+// while (true) {
+//     a = (min + max) / 2; 
+//     a = parseInt(a); 
+
+//     let b = prompt("ur Number > " + a + ", < " + a + " or = " + a + "?");
+
+//     if (b == "=") 
+//     {
+//         alert("Ur number is ->" + a);
+//         break;
+//     } 
+//     else if (b == ">") 
+//     {
+//         min = a + 1; 
+//     } 
+//     else if (b == "<") 
+//     {
+//         max = a - 1; 
+//     } 
+//     else 
+//     {
+//         alert("Input '>', '<' or '=' for answer.");
+//     }
+// }
+
+//4
+// let a;
+// do {
+//     a =parseInt(prompt("Choose ur option: \n1)ml--km\n2)km--ml\n3)EXIT"));
+//     if(a==1) {
+//         let ml=parseInt(prompt("Input milli ->"));
+//         alert("\n1)ml--km == "+ml*1.6)
+//     }
+//     if(a==2) {
+//         let km=parseInt(prompt("Input killometers ->"));
+//         alert("km--ml == "+km/1.6)
+//     }
+// }
+// while(a!=3);
+// alert("EXIT")
